8dadc17a1efea575f9fd197a981263f634960861
Mike Sinkula <mike@mikesinkula.com>
2016-05-17 20:02:55 -0700

    Moved my sub navigation to my functions file.

--- functions.php
+++ functions.php
@@ -20,5 +20,36 @@ register_nav_menus(array(
 add_theme_support('post-thumbnails');
 //
 
+// My Sub Navigation
+function my_subnavigation() {
+
+	if(is_page()) { // if we're in pages
+		
+			if($post->post_parent) { // if the page we're on has a parent
+			
+				echo '<h2>'.get_the_title($post->post_parent).'</h2>';
+				echo '<ul>';
+				wp_list_pages(array('title_li' => '', 'child_of' => $post->post_parent,)); // do this
+				echo '</ul>';
+			
+			} else { // does not have a parent
+				
+				echo '<h2>'.get_the_title($post->ID).'</h2>';
+				echo '<ul>';
+				wp_list_pages(array('title_li' => '', 'child_of' => $post->ID,)); // do this
+				echo '</ul>';
+				
+			}
+		
+		} else { // if we're in postings
+			
+			echo '<h2>Blog</h2>';
+			echo '<ul>';
+			wp_list_categories(array('title_li' => '',));
+			echo '</ul>';
+			
+		}	
+}
+//
 
 ?>
